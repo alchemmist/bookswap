d8d7de2629569d570cdda5962d0aa6dd78f23198
Alchemmist <anton.ingrish@gmail.com>
2025-05-21 09:05:43 +0300

    backend: setup database

--- README.md
+++ README.md
@@ -5,3 +5,56 @@
 ## Ссылка на ТЗ:
 
 https://git.culab.ru/bsc-development-basics-2nd-semester/dev-basics-2025-longreads/-/tree/main/course-project?ref_type=heads
+
+# Database
+
+```mermaid
+erDiagram
+    USERS {
+        UUID id PK
+        TEXT username
+        TEXT password
+        BOOL is_admin
+        TIMESTAMP created_at
+    }
+    BOOKS {
+        UUID id PK
+        TEXT title
+        TEXT author
+        TEXT cover
+        UUID responsable FK
+        TIMESTAMP created_at
+    }
+    REVIEWS {
+        INT id PK
+        TEXT content
+        INT stars
+        UUID reviewer FK
+        UUID book FK
+        TIMESTAMP created_at
+    }
+    PLACES {
+        INT id PK
+        TEXT title
+        TEXT full_address
+    }
+    TRANSFERS {
+        INT id PK
+        UUID sender FK
+        UUID receiver FK
+        BOOL closed
+        INT place FK
+        UUID book FK
+        TIMESTAMP created_at
+        TIMESTAMP closed_at
+    }
+
+    USERS ||--o{ BOOKS     : "responsible for"
+    USERS ||--o{ REVIEWS   : "writes"
+    BOOKS ||--o{ REVIEWS   : "has"
+    USERS ||--o{ TRANSFERS : "sends"
+    USERS ||--o{ TRANSFERS : "receives"
+    BOOKS ||--o{ TRANSFERS : "transferred in"
+    PLACES ||--o{ TRANSFERS: "at"
+```
+
--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,6 +1,45 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    is_admin BOOL NOT NULL DEFAULT false,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS books (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    cover TEXT,
+    responsable UUID NOT NULL REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    stars INTEGER NOT NULL DEFAULT 0,
+    reviewer UUID NOT NULL REFERENCES users(id),
+    book UUID NOT NULL REFERENCES books(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS places (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    full_address TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS transfers (
+    id SERIAL PRIMARY KEY,
+    sender UUID NOT NULL REFERENCES users(id),
+    receiver UUID REFERENCES users(id),
+    closed BOOL NOT NULL DEFAULT false,
+    place INTEGER NOT NULL REFERENCES places(id),
+    book UUID NOT NULL REFERENCES books(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    closed_at TIMESTAMP NOT NULL
 );
 
