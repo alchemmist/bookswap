16806c09d33f65aa376af8f0cd6b904f3b593792
Alchemmist <anton.ingrish@gmail.com>
2025-05-28 13:01:30 +0300

    auth: fix full cors opend

--- auth-service/internal/api/middleware.go
+++ auth-service/internal/api/middleware.go
@@ -1,13 +1,21 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+)
+
+var allowedOrigins = map[string]bool{
+	"http://localhost:3000": true,
+}
 
 func enableCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if origin := r.Header.Get("Origin"); allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
